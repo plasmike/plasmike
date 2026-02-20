2d0909d580601ab36f37cb6d5d557dd969301db7
mike <262139264+plasmike@users.noreply.github.com>
2026-02-20 11:05:42 +0100

    stylistic improvements

--- me.py
+++ me.py
@@ -1,17 +1,18 @@
 class Human:
-  def __init__(self, first_name: str, gender: str, location: str, hobbies: List[str]):
+  def __init__(self, first_name, gender,
+    location, ethnicity, hobbies):
     self.first_name = first_name
     self.gender = gender
     self.location = location
     self.ethnicity = ethnicity
     self.hobbies: List[str] = hobbies
 
-    self.energy: float = 1.0 # 0 = exhausted; 1 = fully awake
+    self.energy: float = 1.0 # 0=tired; 1=awake
     self.alive = True
 
->  def eat(self):
->  def sleep(self):
->  def code(self):
+>  def eat(self): ...
+>  def sleep(self): ...
+>  def code(self): ...
 
 plasmike = Human(
   first_name = "Mike",
